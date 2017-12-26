fb3862c829d6dd44f892bf24f7b3fb163af93bef
Sudeep Pandey <pandey.datatech@gmail.com>
2017-12-26 11:26:56 -0500

    Added Spinner.

--- app/src/main/java/com/vand/capst/capstoneproject_vanderbiltuniversity/MainActivity.java
+++ app/src/main/java/com/vand/capst/capstoneproject_vanderbiltuniversity/MainActivity.java
@@ -20,5 +20,14 @@ public class MainActivity extends Activity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.interests, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner2.setAdapter(adapter2);
     }
 }
