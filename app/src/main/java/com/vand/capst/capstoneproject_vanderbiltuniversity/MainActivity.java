da180b35267b2aebf9abb07a264071d857754bb9
Sudeep Pandey <pandey.datatech@gmail.com>
2017-12-26 11:12:22 -0500

    Added TextView..

--- app/src/main/java/com/vand/capst/capstoneproject_vanderbiltuniversity/MainActivity.java
+++ app/src/main/java/com/vand/capst/capstoneproject_vanderbiltuniversity/MainActivity.java
@@ -2,6 +2,8 @@ package com.vand.capst.capstoneproject_vanderbiltuniversity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.popular_points, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
