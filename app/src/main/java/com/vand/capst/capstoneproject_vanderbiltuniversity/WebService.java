325f5ee8b231818dcc27cbf4ac1929d90e9f6673
Sudeep Pandey <pandey.datatech@gmail.com>
2017-12-28 22:40:19 -0500

    Adjusted layout..

---
+++ app/src/main/java/com/vand/capst/capstoneproject_vanderbiltuniversity/WebService.java
@@ -0,0 +1,51 @@
+package com.vand.capst.capstoneproject_vanderbiltuniversity;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Process;
+
+public class WebService extends Service {
+
+    private Looper mServiceLooper;
+    private ServiceHandler mServiceHandler;
+
+    private final class ServiceHandler extends Handler {
+        public ServiceHandler(Looper looper) {
+            super(looper);
+        }
+    }
+    public WebService() {}
+
+
+
+    @Override
+    public void onCreate(){
+        HandlerThread thread = new HandlerThread("ServiceStartArguments",
+                Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+
+        // Get the HandlerThread's Looper and use it for our Handler
+        mServiceLooper = thread.getLooper();
+        mServiceHandler = new ServiceHandler(mServiceLooper);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void execute(){
+        mServiceHandler.post(new Runnable(){
+           public void run() {
+            //TODO
+           }
+        });
+    }
+
+
+}
