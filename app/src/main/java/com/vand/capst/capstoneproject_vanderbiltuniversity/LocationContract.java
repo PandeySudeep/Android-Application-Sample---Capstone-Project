b99c61634588bd6d6b6f5a14f6a73015fe1d30a1
Sudeep Pandey <pandey.datatech@gmail.com>
2018-01-10 15:05:30 -0500

    RecyclerView

--- app/src/main/java/com/vand/capst/capstoneproject_vanderbiltuniversity/LocationContract.java
+++ app/src/main/java/com/vand/capst/capstoneproject_vanderbiltuniversity/LocationContract.java
@@ -4,30 +4,13 @@ import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
-/**
- * Created by Sudeep.Pandey on 1/7/2018.
- */
 
 public class LocationContract {
 
-
-
-//package vandy.mooc.hobbitcontentprovider.provider;
-
-  //      import android.content.ContentUris;
-    //    import android.net.Uri;
-      //  import android.provider.BaseColumns;
-
-/**
- * This contract defines the metadata for the HobbitContentProvider,
- * including the provider's access URIs and its "database" constants.
- */
-//public final class CharacterContract {
     /**
      * This ContentProvider's unique identifier.
      */
     public static final String CONTENT_AUTHORITY =
-            //"vandy.mooc.hobbitprovider";
     "vand.capst.myprovider";
 
     /**
@@ -38,23 +21,11 @@ public class LocationContract {
             Uri.parse("content://"
                     + CONTENT_AUTHORITY);
 
-    /**
-     * Possible paths (appended to base content URI for possible
-     * URI's), e.g., a valid path for looking at Character data is
-     * content://vandy.mooc.hobbitcontentprovider/character_table .
-     * However, content://vandy.mooc.hobbitcontentprovider/givemeroot
-     * will fail, as the ContentProvider hasn't been given any
-     * information on what to do with "givemeroot".
-     */
     public static final String PATH_LOCATION =
             LocationEntry.TABLE_NAME;
 
-    /*
-     * Columns
-     */
-
     /**
-     * Inner class that defines the table contents of the Hobbit
+     * Inner class that defines the table contents of the data
      * table.
      */
     public static final class LocationEntry
@@ -106,8 +77,8 @@ public class LocationContract {
         /**
          * Columns to store data.
          */
-        public static final String COLUMN_INFO1 = "result_item_1";
-        public static final String COLUMN_INFO2 = "rasult_item_2";
+        public static final String COLUMN_INFO1 = "place_name";
+        public static final String COLUMN_INFO2 = "place_type";
 
         /**
          * Return a Uri that points to the row containing a given id.
